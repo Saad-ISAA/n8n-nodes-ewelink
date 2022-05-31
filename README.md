5b53498d9fa671cbe289bc736d18ea217da3550d
Saad Mujeeb <63791098+Saad-ISAA@users.noreply.github.com>
2022-05-31 09:50:42 +0500

    first commit

---
+++ README.md
@@ -0,0 +1,49 @@
+# n8n-nodes-ewelink
+
+<br />
+<p align="center">
+    <a href="https://n8n.io/" target="_blank"><img width="260" height="" src="https://raw.githubusercontent.com/n8n-io/n8n/master/assets/n8n-logo.png" alt="n8n Logo"></a>
+    <br />
+    <br />
+    <b>Free and open fair-code licensed node based Workflow Automation Tool.</b>
+    <br />
+    <br />
+</p>
+
+<br />
+<p align="center">
+    <a href="https://ewelink.cc/" target="_blank"><img width="50" height="" src="https://ewelink.cc/wp-content/uploads/elementor/thumbs/eWeLink-logo-PNG-512-pmifag8ux4furo10ekzen65hh4uw7vhm7pn5px8r32.png" alt="Ewelink Logo"></a>
+    <br />
+    <br />
+    <b>Pairing, setting, managing all your eWeLink support devices, everything in a single APP</b>
+    <br />
+    <br />
+</p>
+
+<p align="center">
+    <a href="https://n8n.io" target="_blank"><img width="600" height="" src="/n8n-ewelink-node.png" alt="ewelink-node"></a>
+    <br />
+    <br />
+</p>
+
+## How to install
+
+To get started install the package in your n8n root directory with
+
+`npm install n8n-nodes-ewelink`
+
+
+For docker add the following line before the font installation command in your [n8n Docker file](https://github.com/n8n-io/n8n/blob/master/docker/images/n8n/Dockerfile)
+
+
+`RUN cd /usr/local/lib/node_modules/n8n && npm install n8n-nodes-ewelink`
+
+## 🔗 Connect with me
+
+[![linkedin](https://img.shields.io/badge/linkedin-0A66C2?style=for-the-badge&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/saad-mujeeb-b730a860)
+
+[![twitter](https://img.shields.io/badge/twitter-1DA1F2?style=for-the-badge&logo=twitter&logoColor=white)](https://twitter.com/SaadMMujeeb)
+
+## License
+
+[Apache 2.0 with Commons Clause](https://github.com/n8n-io/n8n/blob/master/packages/nodes-base/LICENSE.md)
---
+++ nodes/Ewelink/Ewelink.node.ts
@@ -0,0 +1,198 @@
+import {
+	IExecuteFunctions,
+} from 'n8n-core';
+
+import {
+	ICredentialsDecrypted,
+	ICredentialTestFunctions,
+	IDataObject,
+	NodeCredentialTestResult,
+	INodeExecutionData,
+	INodeType,
+	INodeTypeDescription,
+} from 'n8n-workflow';
+
+import ewelink from 'ewelink-api';
+
+import { deviceFields, deviceOperations } from './DeviceDescription';
+
+export class Ewelink implements INodeType {
+	description: INodeTypeDescription = {
+			displayName: 'Ewelink',
+			name: 'ewelink',
+			icon: 'file:Ewelink.svg',
+			group: ['transform'],
+			version: 1,
+			subtitle: '={{ $parameter["operation"] + ": " + $parameter["resource"] }}',
+			description: 'Consume Ewelink API',
+			defaults: {
+					name: 'Ewelink',
+					color: '#1A82e2',
+			},
+			inputs: ['main'],
+			outputs: ['main'],
+			credentials: [
+				{
+					name: 'ewelinkApi',
+					required: false,
+					testedBy: 'ewelinkApiTest',
+			},
+			],
+			properties: [
+				{
+					displayName: 'Resource',
+					name: 'resource',
+					noDataExpression: true,
+					type: 'options',
+					options: [
+							{
+									name: 'Device',
+									value: 'device',
+							},
+					],
+					default: 'device',
+					required: true,
+					// eslint-disable-next-line n8n-nodes-base/node-param-description-weak
+					description: 'Resource to consume from eweLink-api',
+			},
+			...deviceOperations,
+			...deviceFields,
+		],
+	};
+
+	methods = {
+		credentialTest: {
+			async ewelinkApiTest(this: ICredentialTestFunctions, credential: ICredentialsDecrypted): Promise<NodeCredentialTestResult> {
+				const credentials = await credential.data as IDataObject;
+
+				try {
+					const connection = new ewelink({
+						email: `${credentials.email}`,
+						password: `${credentials.password}`,
+					});
+
+					const loginStatus = await connection.getDevices();
+					//@ts-ignore
+					if(!loginStatus.error){
+						return {
+							status: 'OK',
+							message: 'Authentication successful',
+						};
+					}
+					else {
+						throw new Error('Authentication failed');
+					}
+				} catch (error) {
+					return {
+						status: 'Error',
+						message: `Auth settings are not valid: Authentication failed - status 406`,
+					};
+				}
+			},
+		},
+	};
+	async execute(this: IExecuteFunctions): Promise<INodeExecutionData[][]> {
+		const returnData: IDataObject[] = [];
+
+		let responseData;
+		const resource = this.getNodeParameter('resource', 0) as string;
+		const operation = this.getNodeParameter('operation', 0) as string;
+		const credentials = await this.getCredentials('ewelinkApi') as IDataObject;
+		const connection = new ewelink({
+			email: `${credentials.email}`,
+			password: `${credentials.password}`,
+			// region: 'as',
+		});
+		try {
+			if (resource === 'device') {
+				if (operation === 'getDevices') {
+					responseData = await connection.getDevices();
+					//@ts-ignore
+					returnData.push(responseData);
+				}
+				if (operation === 'getDevice') {
+					const deviceId = this.getNodeParameter('deviceId', 0) as string;
+
+					responseData = await connection.getDevice(deviceId);
+					//@ts-ignore
+					returnData.push(responseData);
+				}
+				if (operation === 'getDevicePowerState') {
+					const deviceId = this.getNodeParameter('deviceId', 0) as string;
+
+					responseData = await connection.getDevicePowerState(deviceId);
+					//@ts-ignore
+					returnData.push(responseData);
+				}
+				if (operation === 'getDeviceChannelCount') {
+					const deviceId = this.getNodeParameter('deviceId', 0) as string;
+
+					responseData = await connection.getDeviceChannelCount(deviceId);
+					//@ts-ignore
+					returnData.push(responseData);
+				}
+				if (operation === 'getDeviceCurrentHumidity') {
+					const deviceId = this.getNodeParameter('deviceId', 0) as string;
+
+					responseData = await connection.getDeviceCurrentHumidity(deviceId);
+					//@ts-ignore
+					returnData.push(responseData);
+				}
+				if (operation === 'getDeviceCurrentTH') {
+					const deviceId = this.getNodeParameter('deviceId', 0) as string;
+
+					responseData = await connection.getDeviceCurrentTH(deviceId);
+					//@ts-ignore
+					returnData.push(responseData);
+				}
+				if (operation === 'getDeviceCurrentTemperature') {
+					const deviceId = this.getNodeParameter('deviceId', 0) as string;
+
+					responseData = await connection.getDeviceCurrentTemperature(deviceId);
+					//@ts-ignore
+					returnData.push(responseData);
+				}
+				if (operation === 'getDevicePowerUsage') {
+					const deviceId = this.getNodeParameter('deviceId', 0) as string;
+
+					responseData = await connection.getDevicePowerUsage(deviceId);
+					//@ts-ignore
+					returnData.push(responseData);
+				}
+				if (operation === 'getFirmwareVersion') {
+					const deviceId = this.getNodeParameter('deviceId', 0) as string;
+
+					responseData = await connection.getFirmwareVersion(deviceId);
+					//@ts-ignore
+					returnData.push(responseData);
+				}
+				if (operation === 'toggleDevice') {
+					const deviceId = this.getNodeParameter('deviceId', 0) as string;
+
+					responseData = await connection.toggleDevice(deviceId);
+					//@ts-ignore
+					returnData.push(responseData);
+				}
+				if (operation === 'setDevicePowerState') {
+					const deviceId = this.getNodeParameter('deviceId', 0) as string;
+					const devicePowerState = this.getNodeParameter('devicePowerState', 0) as boolean;
+
+					responseData = await connection.setDevicePowerState(deviceId, devicePowerState ? 'on' : 'off');
+					//@ts-ignore
+					returnData.push(responseData);
+				}
+			}
+		} catch (error) {
+			if (this.continueOnFail()) {
+				returnData.push({ error: error.message });
+			} else {
+				throw error;
+			}
+		}
+
+		// Map data to n8n data
+		//@ts-ignore
+		return [this.helpers.returnJsonArray(responseData)];
+}
+
+}
